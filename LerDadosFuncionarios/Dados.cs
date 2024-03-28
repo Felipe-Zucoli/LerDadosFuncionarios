c4494594724645dccce052fabf9c05f611fbb318
Felipe-Zucoli <zucolifelipe@gmail.com>
2024-03-28 09:23:41 -0300

    Adicionar arquivos de projeto.

---
+++ LerDadosFuncionarios/Dados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerDadosFuncionarios
+{
+    internal class Dados
+    {
+        public string name;
+        public double salario;
+        public double imposto;
+        public double bonus;
+
+        public double abateImposto()
+        {
+            return (salario - imposto);
+        }
+        public double salarioFinal ()
+        {
+            double d = (abateImposto()/100)*bonus;
+            return abateImposto() + d;
+        }
+        public override string ToString()
+        {
+            return "Nome: "+ name+ ", Salario Bruto: " + salario + ", Imposto: " + imposto+ ", Salário líquido: " + abateImposto() + ", Salario Final: " + salarioFinal();
+        }
+    }
+}
---
+++ LerDadosFuncionarios/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using LerDadosFuncionarios;
+
+class LerDados
+{
+    static void Main(string[] args)
+    {
+        Dados dados = new Dados();  
+
+        Console.WriteLine("Informe o nome do funcionário: ");
+        dados.name = Console.ReadLine();
+
+        Console.WriteLine("Informe o salário do funcionário: ");
+        dados.salario = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o imposto abatido: ");
+        dados.imposto = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o bônus do funcionário: ");
+        dados.bonus = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(dados);
+    }
+}
